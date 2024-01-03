9d1aaa5bb6d139a86b15578b5eaef2f6115185f0
Theophilus Adeleke <theophilusadesola002@gmail.com>
2024-01-03 14:15:05 +0100

    Progress: TOggling focus with javascript

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ function app() {
 	const menuTrigger = document.querySelector("#profile-menu")
 	const menu = document.querySelector("#profile-menu-content")
 	menuTrigger.addEventListener("click", function () {
+		const allMenuItems = document.querySelectorAll('[role="menuitem"]')
+		console.log(allMenuItems)
 		const isExpanded = menuTrigger.attributes["aria-expanded"].value == "true"
 		menu.classList.toggle("menu-active")
 		if (isExpanded) {
 			menuTrigger.ariaExpanded = "false"
+			menuTrigger.focus()
 		} else {
 			menuTrigger.ariaExpanded = "true"
+			allMenuItems.item(0).focus()
 		}
 	})
-	console.log(menu)
 }
 app()
